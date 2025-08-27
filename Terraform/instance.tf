c08c54ef6ab17a294062be2b871a6655b4c0b81d
Gamit Paniel <48850804+Paniel010@users.noreply.github.com>
2025-08-27 21:48:04 +0530

    Update instance.tf

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -1,9 +1,11 @@
-resource "aws_instance" "myserver" {
-    ami = "ami-02d26659fd82cf299"     
+resource "aws_instance" "example" {
+    ami = "ami-02d26659fd82cf299"
         #Ubuntu=ami-02d26659fd82cf299 | AmazonLinux=ami-0861f4e788f5069dd
     instance_type = "t2.micro"
+    security_groups = [ "Terraf-allow" ]
+    key_name = "terraform_key"
 
     tags = {
-        Name = "terraform-instance" #Name of EC-2 Instance
-    } 
+      Name = "ApacheServer"
+    }
 }
